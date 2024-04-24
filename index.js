ecffcff93aea0dc98bd99dfdd7ec8cc064ecef66
ryanmillard <110338235+ryanmillard@users.noreply.github.com>
2024-04-24 23:31:41 +0100

    Initial dist subtree commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const API_KEY = `39e68d119d9e483aa75202618242004`;
+const WEATHER_URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=`;
+
+const locationInput = document.getElementById('location-name');
+
+async function getWeatherData(location) {
+  let url = WEATHER_URL + location;
+  let response = await fetch(url, {mode: 'cors'});
+  
+  if (response.status === 400) {
+    console.log('Place does not exist!');
+    return;
+  }
+
+  let data = await response.json();
+
+  let locationName = document.getElementById('location');
+  let locationExtended = document.getElementById('location-extended');
+  let weatherCondition = document.getElementById('weather-condition');
+  let currentTemp = document.getElementById('current-temp');
+  let feelsLike = document.getElementById('feels-like');
+  let windSpeed = document.getElementById('wind-speed');
+  let humidity = document.getElementById('humidity');
+
+  locationName.textContent = data.location.name;
+  locationExtended.textContent = `${data.location.region}, ${data.location.country}`;
+  weatherCondition.textContent = data.forecast.forecastday[0].day.condition.text.toUpperCase();
+  currentTemp.textContent = parseInt(data.current.temp_c);
+  feelsLike.textContent = `Feels like: ${parseInt(data.current.feelslike_c)}ºC`;
+  windSpeed.textContent = `Wind Speed: ${parseInt(data.current.wind_mph)} MPH`;
+  humidity.textContent = `Humidity: ${parseInt(data.current.humidity)}%`;
+
+  console.log("Data:", data);
+}
+
+document.forms['get_weather'].addEventListener('submit', (e) => {
+  e.preventDefault();
+  getWeatherData(locationInput.value).catch((e) => console.log("KWADPOAKSPOD"));
+});
+
+getWeatherData('london');
